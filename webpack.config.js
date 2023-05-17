b8bccf3bb2c9975ae4d2862ada8bba3a1b2f62bc
Jay <jpark@mymoneymyfuture.co>
2023-05-17 01:06:45 -0700

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,14 @@ module.exports = {
   experiments: {
     topLevelAwait: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/preset-react"] },
+      },
+    ],
+  },
 };
